1a8eda28a748a5cbbcaa1a7933edaa9a69239745
Potpourri Fajilagot <151165406+Mss-pri17@users.noreply.github.com>
2024-03-04 12:16:04 -0500

    2 functions (inputShipmentDetails and displayInvalidDestination)

---
+++ root/SourceCode/Potpourri.h
@@ -0,0 +1,10 @@
+#pragma once
+// milestone2 header file to create data structures
+// please put the comment with your individual functions you created
+
+
+//This function prompts the user to enter shipment details and validate the inputs provided by the user. 
+int inputShipmentDetails(int weight, float boxSize, char destination);
+
+//This function displays a message indicating that the destination entered by the user is invalid
+void displayInvalidDestination();
