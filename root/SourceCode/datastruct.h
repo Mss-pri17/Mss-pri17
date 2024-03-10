3c865f252de47ed0dafd9a229a3fb1964f6ce8f2
Janice Kang <jkang108@myseneca.ca>
2024-03-10 11:11:11 -0400

    update

--- root/SourceCode/datastruct.h
+++ root/SourceCode/datastruct.h
@@ -1,3 +1,7 @@
 #pragma once
 // milestone2 header file to create data structures
 // please put the comment with your individual functions you created
+
+void displayInvalidSize(); // Janice
+
+void displayInvalidWeight(); // Janice 
