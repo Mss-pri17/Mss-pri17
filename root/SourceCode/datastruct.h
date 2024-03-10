0e26aba4c185a19ab1bc8276b7e3b4a69bffe1ee
Janice Kang <jkang108@myseneca.ca>
2024-03-10 11:28:20 -0400

    datastruct.h file updated

--- root/SourceCode/datastruct.h
+++ root/SourceCode/datastruct.h
@@ -3,13 +3,16 @@
 // please put the comment with your individual functions you created
 
 <<<<<<< HEAD
+// To display an error message for invalid size
 void displayInvalidSize(); // Janice
 
+// To display an error message for invalid weight
 void displayInvalidWeight(); // Janice 
 =======
 //This function prompts the user to enter shipment details and validate the inputs provided by the user. 
-int inputShipmentDetails(int weight, float boxSize, char destination);
+int inputShipmentDetails(int weight, float boxSize, char destination); // By Potpourri 
 
-//This function displays a message indicating that the destination entered by the user is invalid
-void displayInvalidDestination();
+//This function displays a message indicating that the destination entered by the user is invalid 
+void displayInvalidDestination(); // By Potpourri 
 >>>>>>> ab1340d6cdc52ee5cd4f78d2e012438615afb035
+
